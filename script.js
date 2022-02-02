3b00e553b7d0810a487659fd293d66f4ce7792cc
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2022-02-02 10:47:02 +0100

    more

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 window.onload = choosePic;
 
-// var myPix = new Array("images/lion.jpg","images/tiger.jpg","images/bear.jpg");
-const pics = new Array("nhoco-nhoco.webp",);
+const pics = [
+    "img/2d1fc437-d172-4a13-9120-b9003cf806ba.webp",
+    "img/6b99668f-f3e6-4d45-90f8-36564befe02d.webp",
+    "img/19d4b97d-e4e6-4fb0-9cf9-df40db84e444.webp",
+    "img/39ff4cb3-3cc9-41a8-ab2a-dae7a0a3586a.webp",
+    "img/91a3e2e9-1070-40cb-89e3-4c377451734a.webp",
+    "img/244f8ac5-c756-4378-a13c-7d33122a0727.webp",
+    "img/816d7603-f423-4e91-9c9b-90c3a95c124c.webp",
+    "img/76255d4c-8731-41f9-bd73-fa96edf1005b.webp",
+    "img/a38709f9-f198-416f-b7c5-1e9bda9de408.webp",
+    "img/fruuuu.webp",
+];
 
 function choosePic() {
     const randomNum = Math.floor(Math.random() * pics.length);
